4258f17b8e804f14833e585e7b04b87500952786
siddhi_1305 <siddhisharma4549@gmail.com>
2024-11-04 22:25:33 +0530

    added some more folders for sidebars

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from "raect";
+
+import {navLinks} from '../constants';
+import {sun} from "../assets";
+
+const Icon = ({style, name, imageUrl, isActive, disabled, handleClick}) => {
+    return (
+        <div
+        className = {'h-[48px] w-[48px]'}>
+
+        </div>
+    )
+}
